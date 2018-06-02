1f76fb0b87a9d0be57bcb44b50ca2d88f91112fb
inaaa <inch.binch@gmail.com>
2018-06-02 18:20:39 +0200

    create a simple django app

---
+++ courses/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from .models import Course
+
+# Create your views here.
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'courses/course_list.html', {'courses': courses})
+
