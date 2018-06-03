212f9228f749e8b24e82075de57d696ae14c7724
inaaa <inch.binch@gmail.com>
2018-06-03 12:48:37 +0200

    Add a second Model in relation many to one to Courses. Config the static dir.

--- explore_django/urls.py
+++ explore_django/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, re_path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -23,3 +24,5 @@ urlpatterns = [
     re_path(r'^courses/', include('courses.urls')),
     re_path(r'^$', views.home),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
