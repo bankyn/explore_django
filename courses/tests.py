d56c4683fca7d38fbdc4f5713c10ccb13e0193b0
bankyn <inch.binch@gmail.com>
2018-06-05 17:06:38 +0200

    Start with testing

--- courses/tests.py
+++ courses/tests.py
@@ -1,7 +1,9 @@
+from django.urls import reverse
 from django.test import TestCase
 from django.utils import timezone
-from .models import Course
+from .models import Course, Step
 
+### test Models, Views, URLs, Templates
 
 class CourseModelTests(TestCase):
     def test_course_creation(self):
@@ -10,4 +12,37 @@ class CourseModelTests(TestCase):
             description="Learn to write regex in Python"
         )
         now = timezone.now()
-        self.assertLess(course.created_at, now)
+        self.assertLessEqual(course.created_at, now)
+
+
+class StepModelTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Testing",
+            description="Learn to write tests in Python"
+        )
+    def test_step_creation(self):
+        step = Step.objects.create(
+            title="Doctests",
+            description="Learn to write tests in docstrings.",
+            course = self.course
+        )
+        self.assertIn(step, self.course.step_set.all())
+
+class CourseViewsTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Testing",
+            description="Learn to write tests in Python"
+        )
+        self.course2 = Course.objects.create(
+            title="New Course",
+            description="A new course"
+        )
+        self.step = Step.objects.create(
+            title="Introduction to Doctests",
+            description="Learn to write tests in docstrings.",
+            course=self.course
+        )
+
+
