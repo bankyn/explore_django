5319337486cc23e0bd4a88185be22272a8257358
bankyn <inch.binch@gmail.com>
2018-06-18 17:08:16 +0200

    Bugfix view funktion and url

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "courses"
 urlpatterns = [
     re_path(r'^$', views.course_list, name='list'),
-    re_path(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.course_detail,
+    re_path(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail,
             name='step'),
     re_path(r'(?P<pk>\d+)/$', views.course_detail, name='detail')
 ]
