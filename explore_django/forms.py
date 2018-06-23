1d559500691f377cd1a55582b191b30c82ceb9e1
bankyn <inch.binch@gmail.com>
2018-06-23 19:42:16 +0200

    Add new form and form validators

---
+++ explore_django/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.core import validators
+
+def must_be_empty(value):
+    if value:
+        raise forms.ValidationError('is not empty')
+
+class SuggestionForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    verify_email = forms.EmailField(label='Verify your email address')
+    suggestion = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(
+        required=False,
+        widget=forms.HiddenInput,
+        label="Leave empty",
+        validators=[must_be_empty]
+ # 2nd version of validating      validators=[validators.MaxLengthValidator(0)]
+    )
+    '''
+    3rd version of validating
+    def clean_honeypot(self):
+        honeypot = self.cleaned_data['honeypot']
+        if len(honeypot):
+            raise forms.ValidationError(
+                'Honeypot should be left empty, bad bot!')
+        return honeypot
+    '''
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data['email']
+        verify = cleaned_data['verify_email']
+
+        if email != verify:
+            raise forms.ValidationError('You need to enter the same email in both fields')
--- explore_django/views.py
+++ explore_django/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
+from . import forms
 
 def homepage(request):
     return render(request, 'home.html')
+
+def suggestion_view(request):
+    form = forms.SuggestionForm()
+    if request.method == 'POST':
+        form = forms.SuggestionForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                'Suggestion from {}'.format(form.cleaned_data['name']),
+                form.cleaned_data['suggestion'],
+                '{name} <{email}>'.format(**form.cleaned_data),
+                ['kenneth@teamtreehouse.com']
+            )
+            messages.add_message(request, messages.SUCCESS,
+                                 'Thanks for your suggestion!')
+            return HttpResponseRedirect(reverse('suggestion'))
+    return render(request, 'suggestion_form.html', {'form': form})
