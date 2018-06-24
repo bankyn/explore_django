0d123d69c2a682bd6af3b615dc0db8750b650504
bankyn <inch.binch@gmail.com>
2018-06-24 21:12:22 +0200

    Add new Models, use both abstract and mti

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,10 +15,68 @@ class Step(models.Model):
     description = models.TextField()
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    content = models.TextField(blank=True, default='')
 
     class Meta:
         ordering = ['order',]
+        abstract = True
 
     def __str__(self):
         return self.title
+
+
+class Text(Step):
+    content = models.TextField(blank=True, default='')
+
+    def get_absolute_url(self):
+        return reverse('courses:text', kwargs={
+            'course_pk': self.course_id,
+            'step_pk': self.id
+        })
+
+class Quiz(Step):
+    questions = models.IntegerField(default=4)
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+    def get_absolute_url(self):
+        return reverse('courses:quiz', kwargs={
+            'course_pk': self.course_id,
+            'step_pk': self.id
+        })
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    order = models.IntegerField(default=0)
+    prompt = models.TextField()
+
+    class Meta():
+        ordering = ['order',]
+
+    def get_absolute_url(self):
+        return self.quiz.get_absolute_url()
+
+    def __str__(self):
+        return self.prompt
+
+
+class MultipleChoiseQuestion(Question):
+    shuffle_answers = models.BooleanField(default=False)
+
+
+class TrueFalseQuestion(Question):
+   pass
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    order = models.IntegerField(default=0)
+    text = models.TextField(max_length=255)
+    correct = models.BooleanField(default=False)
+
+    class Meta():
+        ordering = ['order',]
+
+    def __str__(self):
+        return self.text
--- courses/urls.py
+++ courses/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = "courses"
 urlpatterns = [
     re_path(r'^$', views.course_list, name='list'),
-    re_path(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail,
-            name='step'),
+    re_path(r'(?P<course_pk>\d+)/q(?P<step_pk>\d+)/$', views.quiz_detail,
+            name='quiz'),
+    re_path(r'(?P<course_pk>\d+)/t(?P<step_pk>\d+)/$', views.text_detail,
+            name='text'),
     re_path(r'(?P<pk>\d+)/$', views.course_detail, name='detail')
 ]
 
