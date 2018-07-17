83da745be40c13afad748009fe4e8cf4e693e51c
bankyn <inch.binch@gmail.com>
2018-07-17 20:23:29 +0200

    Improve queries

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q, Count, Sum
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from itertools import chain
 
 from . import models
@@ -9,14 +10,30 @@ from . import forms
 
 
 def course_list(request):
-    courses = models.Course.objects.filter(published=True)
-    return render(request, 'courses/course_list.html', {'courses': courses})
+    courses = models.Course.objects.filter(
+        published=True
+    ).annotate(
+        total_steps=Count('text', distinct=True)+Count('quiz', distinct=True)
+    )
+    total = courses.aggregate(total=Sum('total_steps'))
+    return render(request, 'courses/course_list.html', {
+        'courses': courses,
+        'total': total
+    })
 
 
 def course_detail(request, pk):
-    course = get_object_or_404(models.Course, pk=pk, published=True)
-    steps = sorted(chain(course.text_set.all(), course.quiz_set.all()),
-                   key=lambda step: step.order)
+    try:
+        course = models.Course.objects.prefetch_related(
+            'quiz_set','text_set', 'quiz_set__question_set'
+        ).get(pk=pk, published=True)
+    except models.Course.DoesNotExist:
+        raise Http404
+    else:
+        steps = sorted(chain(
+            course.text_set.all(),
+            course.quiz_set.all()
+        ), key=lambda step: step.order)
     return render(request, 'courses/course_detail.html', {
         'course': course,
         'steps': steps})
@@ -29,9 +46,21 @@ def text_detail(request, course_pk, step_pk):
 
 
 def quiz_detail(request, course_pk, step_pk):
-    step = get_object_or_404(models.Quiz, course_id=course_pk, pk=step_pk,
-                             course__published=True)
-    return render(request, 'courses/quiz_detail.html', {'step': step})
+    try:
+        step = models.Quiz.objects.select_related(
+            'course'
+        ).prefetch_related(
+            'question_set',
+            'question_set__answer_set'
+        ).get(
+            course_id=course_pk,
+            pk=step_pk,
+            course__published=True
+        )
+    except models.Quiz.DoesNotExist:
+        raise Http404
+    else:
+        return render(request, 'courses/quiz_detail.html', {'step': step})
 
 
 @login_required
@@ -179,6 +208,9 @@ def courses_by_teacher(request, teacher):
 
 def search(request):
     term = request.GET.get('q')
-    courses = models.Course.objects.filter(title__icontains=term,
-                                           published=True)
+    courses = models.Course.objects.filter(
+        Q(title__icontains=term)|Q(description__icontains=term),
+        published=True
+    )
+
     return render(request, 'courses/course_list.html', {'courses': courses})
